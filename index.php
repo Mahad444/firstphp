2f647c4e683b1ed79bd59bac36d0d9289cdb0e4f
Mahad444 <mahadsaid95@icloud.com>
2023-01-11 18:04:28 +0300

    Practing variables

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-welcome to Php class
+<!-- welcome to Php class -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$num1 = 10;
+$num2 = 15 ;
+
+$result = $num1 + $num2;
+
+echo "the answer is ".$result;
+
+$text = "myname";
+
+echo strtoupper($text) ;
+$num3 = 10;
+ $num3 ++;
+ echo $num3;
+
+
+echo str_replace('a','w', $text)
+?>
+</body>
+</html>
