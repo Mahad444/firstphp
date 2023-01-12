155a363a770d456c60fed22abd8e819cd11d186d
Mahad444 <mahadsaid95@icloud.com>
2023-01-12 15:11:15 +0300

    learning conditional and loops

--- mahad.php
+++ mahad.php
@@ -1,1 +1,39 @@
-my school is IST
+<?php
+
+$age = 18;
+
+if ($age >= 18)
+{
+    echo "you 18yrs and above "
+}
+
+elseif($age<18)
+{
+    echo("you are not 18yrs");
+}else($age){
+    echo("you are 18yrs");
+}
+// SITCH
+$age2=
+?>
+
+<!-- LOOPS -->
+<!-- for Loop -->
+<?php
+for ($i= 1; $i<=10; $i++){
+    echo i;
+}
+?>
+<!-- While Loop -->
+<?php
+$i=i;
+while($i<=10){
+    echo i
+}
+?>
+<!-- do while -->
+<?php
+do{
+    
+}
+?>
