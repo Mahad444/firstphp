881e142bd7ce2ef8210135aeb0c3e5254af90d68
Mahad444 <mahadsaid95@icloud.com>
2023-01-16 13:06:11 +0300

    Learnig datatypes

---
+++ datatypes.php
@@ -0,0 +1,36 @@
+<?php
+// Integers
+
+$number = 6;
+var_dump($number);
+echo gettype($number);
+
+#gettype says exactly the type of that datatype
+
+// Floats
+
+echo "<br>";
+$numberFloat = 6.89;
+ var_dump($numberFloat);
+
+//  Booleans
+echo "<br>";
+
+$bool = true;
+var_dump($bool);
+
+// variables(strings)
+
+echo "<br>";
+ $var = "this is a sting";
+ var_dump($var);
+
+//  Null
+echo "<br>";
+$vary = "this a value";
+$vary = null;
+#Null Can Overwrite The Variables
+
+var_dump($vary);
+
+?>
