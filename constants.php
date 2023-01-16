e0fce3af719bba1f307d161ea9d55b953da489c7
Mahad444 <mahadsaid95@icloud.com>
2023-01-16 10:18:02 +0300

    creating constant using 	rue boolean

--- constants.php
+++ constants.php
@@ -3,4 +3,15 @@
 <?php
 define("CONSTANT","This is a constant",false);
 echo CONSTANT;
+
+/*define("NUMBERS",99);
+echo numbers;*/ 
+
+#the above code brings out errors due to case sensitivity
+
+define("NUMBERS",99,true);
+echo numbers;
+#the code solves the case sensitivity by using `true` boolean
+
+
 ?>
